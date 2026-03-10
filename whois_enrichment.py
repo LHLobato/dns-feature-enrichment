03279ae71b2657d52d3cf893e9fd422690d457e3
LHLobato <luislobato35@gmail.com>
2026-03-10 10:06:35 -0300

    whois batch implemented

--- whois_enrichment.py
+++ whois_enrichment.py
@@ -5,20 +5,29 @@ from concurrent.futures import ThreadPoolExecutor
 import time
 import random 
 from common_functions import get_whois_features
+from tqdm import tqdm 
+import os 
 
 df = pd.read_csv("dataset.csv")
-df = df[:232839]
+df = df.head(232839)
 dominios = df['name'].values
 resultados = []
 
+csv_file = 'whois-query-result.csv'
 
-with ThreadPoolExecutor(max_workers=4) as executor:
-    resultados = list(executor.map(get_whois_features, dominios))
+batch_size = 5000
+batch_idx = 0 
+
+for i in tqdm(range(0, len(dominios), batch_size), desc="Consultando WHOIS em Batches"):
+    domain_batch = dominios[i:i+batch_size]
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        resultados = list(executor.map(get_whois_features, domain_batch))
+    resultados = pd.DataFrame(resultados)
+    resultados["name"] = domain_batch
+        
+    file_exists = os.path.isfile(csv_file)
+    resultados.to_csv(csv_file, mode='a', index=False, header=not file_exists)
     
-resultados = pd.DataFrame(resultados)
-resultados["name"] = dominios
 
-df = df.merge(resultados, on="name", how="left")
-df.to_csv("part1-whois-enriched.csv", index=False)
-print("Salvo em part1-whois-enriched.csv!")
+
 
